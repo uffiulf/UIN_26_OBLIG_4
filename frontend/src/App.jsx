9af7f0e1aa2b30d611851e5474292487e8d8951a
GalaxMax <maxoliver04@gmail.com>
2026-04-22 13:53:34 +0200

    Første iterasjon av kort

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Assignments from './components/Assignments'
+import ProfileCardMax from './components/ProfileCardMax'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Header />
+    <ProfileCardMax />
     <Assignments />
     </>
   )
