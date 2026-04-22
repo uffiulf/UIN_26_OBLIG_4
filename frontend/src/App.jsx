812573166108c84d8c15036b012717e17de655f6
uffiulf <sofnbeat@gmail.com>
2026-04-22 16:26:20 +0200

    lagt til profil + bilde + css med superfin styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Assignments from './components/Assignments'
+import OlavProfil from './components/olavprofil'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Header />
+    <OlavProfil />
     <Assignments />
     </>
   )
