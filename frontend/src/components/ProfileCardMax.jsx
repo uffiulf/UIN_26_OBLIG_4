36faba93386ac55aa5dc78e711bd168f07a3a7ff
GalaxMax <maxoliver04@gmail.com>
2026-04-22 14:25:28 +0200

    Andre utkast av profilkort

--- frontend/src/components/ProfileCardMax.jsx
+++ frontend/src/components/ProfileCardMax.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import '../styling/CardDesignMax.css'
 
 export default function ProfileCardMax() {
-    const [clickedBoolean, setClickedBoolean] = useState(false)
 
     return (
         <section className="profile-card-max" onClick={() => setClickedBoolean(!clickedBoolean)}>
             <h2>Max Oliver</h2>
+            <img src="/kalle.jpeg" alt="Kalle Kanin" />
             <a href="mailto:maxos@hiof.no">maxos@hiof.no</a>
-            {clickedBoolean && <img src="https://placehold.co/400x600/png" alt="Placeholder" />}
-            
+
             <p>Studerer Informatikk - design og utvikling av IT-systemer</p>
+            
         </section>
     )
 }
